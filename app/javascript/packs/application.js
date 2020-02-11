60efe0f98483f59e9b43a8ac9c9feca6f35018ee
Ben Butler <benbutler08@gmail.com>
2020-02-11 07:16:56 -0500

    refactor js for assignments index page into jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,8 @@ require("@rails/actiontext")
 import '../stylesheets/application'
 import './bootstrap_custom.js'
 
-
+window.jQuery = $;
+window.$ = $;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
